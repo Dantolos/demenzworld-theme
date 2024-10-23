9f65e098bb14ebd7e702a82a8730093cb5e87cb8
Dantolos <45291569+Dantolos@users.noreply.github.com>
2024-10-23 16:49:58 +0200

    1.1.09

--- page.php
+++ page.php
@@ -8,7 +8,7 @@ get_header();
 ?>
 <div class="dw__page_wrapper dw_default_page" >  
      <div class="dw__page_content_container dw_default_page_content"> 
-          <?php echo get_the_content(); ?>
+          <?php echo the_content(); ?>
      </div>
 </div>
 
